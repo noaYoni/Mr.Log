4cb601885fd67103a11e2d6a35f3785f273b1818
noaYoni <noa833@gmail.com>
2017-06-15 02:27:33 +0300

    noa

--- app/src/main/java/com/example/sapir/logmaron/addEvent.java
+++ app/src/main/java/com/example/sapir/logmaron/addEvent.java
@@ -1,7 +1,9 @@
 package com.example.sapir.logmaron;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class addEvent extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class addEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+    }
+    public void AddEvent(View aview) {
+        Intent i = new Intent(getApplicationContext(), Notification.class);
+        startActivity(i);
     }
 }
