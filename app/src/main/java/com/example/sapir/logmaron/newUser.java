f6e4c5a595a9e404ba2d1347d006a4381bc1869d
Karin <karindavidov7896@gmail.com>
2017-06-15 01:15:11 +0300

    Karin

--- app/src/main/java/com/example/sapir/logmaron/newUser.java
+++ app/src/main/java/com/example/sapir/logmaron/newUser.java
@@ -2,6 +2,9 @@ package com.example.sapir.logmaron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class newUser extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class newUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+    }
+    public void onSignUp() {
+
+        EditText name = (EditText)findViewById(R.id.FirstName);
+//        Editable email = emailEditText.getText();
+        Toast.makeText(this, name.getText() , Toast.LENGTH_SHORT).show();
+
+//        Intent i = new Intent(getApplicationContext(),Notification.class);
+//        startActivity(i);
     }
 }
